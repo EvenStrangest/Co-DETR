189d56c432a485619fdc6cb22390d32afc2c2422
aelron <asaf.elron@gmail.com>
2024-09-23 09:16:24 +0300

    first try on create ms coco data on clearml

---
+++ data/create_mc_coco_on_clearml.py
@@ -0,0 +1,21 @@
+import os
+from clearml import Dataset
+
+coco_path = "C:/Users/asafe/PycharmProjects/data/coco/"
+
+# Create a dataset with ClearML`s Dataset class
+dataset = Dataset.create(
+    dataset_project="MS_COCO", dataset_name="MS_COCO_2017"
+)
+
+dataset.add_files(path=os.path.join(coco_path, "annotations/instances_val2017.json"), local_base_folder=coco_path,
+                  verbose=True)
+dataset.add_files(path=os.path.join(coco_path, "val2017/"), local_base_folder=coco_path,
+                  verbose=False)
+
+# Upload dataset to ClearML server
+dataset.upload()
+
+# commit dataset changes
+dataset.finalize()
+
