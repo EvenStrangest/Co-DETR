805851e8f81061bddad5381f92ad9cfcf8efabb8
aelron <asaf.elron@gmail.com>
2024-09-23 09:28:41 +0300

    first try on create robot A data on clearml

---
+++ data/create_robotA_on_clearml.py
@@ -0,0 +1,21 @@
+import os
+from clearml import Dataset
+
+local_root_path = r"H:\Shared drives\RnD\Data\Robot-controlled A"
+
+# Create a dataset with ClearML`s Dataset class
+dataset = Dataset.create(
+    dataset_project="SurgicalTools", dataset_name="RobotA"
+)
+
+dataset.add_files(path=os.path.join(local_root_path, r"as_yolo/images"), local_base_folder=local_root_path,
+                  verbose=False)
+dataset.add_files(path=os.path.join(local_root_path, "as_yolo_as_coco_annotations.json"), local_base_folder=local_root_path,
+                  verbose=True)
+
+# Upload dataset to ClearML server
+dataset.upload()
+
+# commit dataset changes
+dataset.finalize()
+
