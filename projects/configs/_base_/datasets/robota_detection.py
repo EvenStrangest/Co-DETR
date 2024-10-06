f6714c1bde676eb433ac4238b8edf120acda3a4d
aelron <asaf.elron@gmail.com>
2024-10-06 20:51:36 +0300

    slowly but surely, gah!

--- projects/configs/_base_/datasets/robota_detection.py
+++ projects/configs/_base_/datasets/robota_detection.py
@@ -1,8 +1,11 @@
 # dataset settings
-dataset_type = 'Robot-ControlledADataset'
-data_root = 'data/robota/'
+dataset_type = 'CocoDataset'
+data_root = 'data/Robot-controlled A/'
+
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+# TODO: what should these be?
+
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
@@ -28,22 +31,24 @@ test_pipeline = [
             dict(type='Collect', keys=['img']),
         ])
 ]
+
 data = dict(
     samples_per_gpu=2,
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_train2017.json',
-        img_prefix=data_root + 'train2017/',
+        ann_file=data_root + 'as_yolo_as_coco_annotations.json',
+        img_prefix=data_root + 'as_yolo/images',
         pipeline=train_pipeline),
     val=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
+        ann_file=data_root + 'as_yolo_as_coco_annotations.json',
+        img_prefix=data_root + 'as_yolo/images',
         pipeline=test_pipeline),
-    test=dict(
-        type=dataset_type,
-        ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
-        pipeline=test_pipeline))
+    # test=dict(
+    #     type=dataset_type,
+    #     ann_file=data_root + 'annotations/instances_val2017.json',
+    #     img_prefix=data_root + 'val2017/',
+    #     pipeline=test_pipeline)
+)
 evaluation = dict(interval=1, metric='bbox')
