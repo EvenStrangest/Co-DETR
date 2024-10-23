34486087f24b8116755188ee3a04eca88c794940
Elron <EvenStrangest@mentor.org.il>
2024-10-23 07:54:00 +0300

    setup labA dataset [never run!]

--- projects/configs/_base_/datasets/laba_detection.py
+++ projects/configs/_base_/datasets/laba_detection.py
@@ -1,23 +1,25 @@
 # dataset settings
 dataset_type = 'RobotaDataset'
-data_root = 'data/Robot-controlled A/'
+data_root = 'data/Lab A/'
+
+# TODO: consider importing img_norm_cfg (and other commonalities) from robota_detection.py
 
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)  # TODO: what should these be?
 # background_color = ()
 
-train_pipeline = [
-    dict(type='LoadImageFromFile'),
-    dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
-    dict(type='RandomFlip', flip_ratio=0.5),  # TODO: consider specifying direction=['horizontal', 'vertical']
-    # TODO: consider adding a Rotate augmentation; specify img_fill_val=background_color
-    # TODO: consider adding a Translate augmentation; specify img_fill_val=background_color
-    dict(type='Normalize', **img_norm_cfg),
-    dict(type='Pad', size_divisor=32),
-    dict(type='DefaultFormatBundle'),
-    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
-]
+# train_pipeline = [
+#     dict(type='LoadImageFromFile'),
+#     dict(type='LoadAnnotations', with_bbox=True),
+#     dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+#     dict(type='RandomFlip', flip_ratio=0.5),  # TODO: consider specifying direction=['horizontal', 'vertical']
+#     # TODO: consider adding a Rotate augmentation; specify img_fill_val=background_color
+#     # TODO: consider adding a Translate augmentation; specify img_fill_val=background_color
+#     dict(type='Normalize', **img_norm_cfg),
+#     dict(type='Pad', size_divisor=32),
+#     dict(type='DefaultFormatBundle'),
+#     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+# ]
 test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
@@ -37,20 +39,20 @@ test_pipeline = [
 data = dict(
     samples_per_gpu=2,
     workers_per_gpu=2,
-    train=dict(
-        type=dataset_type,
-        ann_file=data_root + 'as_yolo_as_coco_ann_train.json',
-        img_prefix=data_root + 'as_yolo/images/',
-        pipeline=train_pipeline),
-    val=dict(
-        type=dataset_type,
-        ann_file=data_root + 'as_yolo_as_coco_ann_val.json',
-        img_prefix=data_root + 'as_yolo/images/',
-        pipeline=test_pipeline),
+    # train=dict(
+    #     type=dataset_type,
+    #     ann_file=data_root + 'as_yolo_as_coco_ann_train.json',
+    #     img_prefix=data_root + 'as_yolo/images/',
+    #     pipeline=train_pipeline),
+    # val=dict(
+    #     type=dataset_type,
+    #     ann_file=data_root + 'as_yolo_as_coco_ann_val.json',
+    #     img_prefix=data_root + 'as_yolo/images/',
+    #     pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'as_yolo_as_coco_ann_test.json',
-        img_prefix=data_root + 'as_yolo/images/',
+        ann_file=data_root + 'raw_images.json',
+        img_prefix=data_root + 'raw_images/',
         pipeline=test_pipeline)
 )
 evaluation = dict(interval=1, metric='bbox')
