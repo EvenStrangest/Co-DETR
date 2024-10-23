ca4abf9086c9f48a2b54ed39fa24759185e1f86b
aelron <asaf.elron@gmail.com>
2024-10-23 10:18:51 +0300

    minor

--- data/create_labA_on_clearml.py
+++ data/create_labA_on_clearml.py
@@ -7,7 +7,7 @@ local_root_path = r"H:\Shared drives\RnD\Data\Lab A"
 
 # Create a dataset with ClearML`s Dataset class
 dataset = Dataset.create(
-    dataset_project="SurgicalTools", dataset_name="LabA"
+    dataset_project="SurgicalTools", dataset_name="LabA", dataset_version="1.0.0"
 )
 
 dataset.add_files(path=os.path.join(local_root_path, r"raw_images"), local_base_folder=local_root_path,
