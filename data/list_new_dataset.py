da504296a9255ff667ac91b390f1e67abbc224e4
Elron <EvenStrangest@mentor.org.il>
2024-10-23 12:39:47 +0300

    list_new_dataset.py used properly

--- data/list_new_dataset.py
+++ data/list_new_dataset.py
@@ -1,6 +1,8 @@
 # https://chatgpt.com/share/67188e15-3b80-8003-b0e6-a39dd3fb3ed2
 # NOTE: PyLabel fails to correctly export to MS COCO format images with nothing but background, so we'll do without
 
+import os
+
 from tools.dataset_converters.images2coco import collect_image_infos, cvt_to_coco_json
 import mmcv
 
@@ -12,6 +14,12 @@ annotations_path = r"H:\Shared drives\RnD\Data\Lab A\raw_images.json"
 print(f'Collecting image infos from {images_path}')
 img_infos = collect_image_infos(images_path)
 
+for img_info in img_infos:
+    img_info['filename'] = os.path.split(img_info['filename'])[1]
+
+# sort by filename
+img_infos = sorted(img_infos, key=lambda x: x['filename'])
+
 # 2 convert to coco format data
 classes = [f'AR{i:02}' for i in range(1, 16)]  # TODO: is this really correct?
 print(f"Classes: {classes}")
@@ -19,5 +27,5 @@ coco_info = cvt_to_coco_json(img_infos, classes)
 
 # 3 dump
 print(f'save json file: {annotations_path}')
-mmcv.dump(coco_info, annotations_path)
+mmcv.dump(coco_info, annotations_path, indent=4)
 
