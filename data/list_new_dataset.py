e16e7c5c60ed26590df4a74980dc1b0ef9cf7206
Elron <EvenStrangest@mentor.org.il>
2024-11-19 15:23:43 +0200

    source_over_usb_as_coco_annotations.json

--- data/list_new_dataset.py
+++ data/list_new_dataset.py
@@ -7,8 +7,8 @@ from tools.dataset_converters.images2coco import collect_image_infos, cvt_to_coc
 import mmcv
 
 
-images_path = r"H:/Shared drives/RnD/Data/Lab C/crop_one"
-annotations_path = r"H:/Shared drives/RnD/Data/Lab C/crop_one_as_coco_annotations.json"
+images_path = r"H:/Shared drives/RnD/Data/Lab D/source_over_usb"
+annotations_path = r"H:/Shared drives/RnD/Data/Lab D/source_over_usb_as_coco_annotations.json"
 
 # 1 load image list info
 print(f'Collecting image infos from {images_path}')
