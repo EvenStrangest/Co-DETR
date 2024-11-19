4013b28a40f40f22cb3b3e232f0e5af37efe5cf9
Elron <EvenStrangest@mentor.org.il>
2024-11-19 17:21:22 +0200

    meaningful RobotA names

--- projects/datasets/robota.py
+++ projects/datasets/robota.py
@@ -5,5 +5,15 @@ from mmdet.datasets.builder import DATASETS
 @DATASETS.register_module()
 class RobotaDataset(CocoDataset):
 
-    CLASSES = ('AR01', 'AR10', 'AR12', 'AR13', 'AR02', 'AR03', 'AR05', 'AR06', 'AR07', 'AR09', 'AR14')
+    CLASSES = ('AR01 Foerster Sponge',
+               'AR10',
+               'AR12',
+               'AR13',
+               'AR02 Metzenbaum-Nelson Scissors',
+               'AR03 Dissecting Scissors 23.0',
+               'AR05 Metzenbaum Scissors, straight 20.0',
+               'AR06 POTTS-SMITH  19.1',
+               'AR07 Mayo Scissors 17.3',
+               'AR09 Lahey Traction Forceps 15.8',
+               'AR14 Septum Forceps Hartman 20.0')
 
