7e50381451553c22d6f0239a28a98a61c66ea988
aelron <asaf.elron@gmail.com>
2024-11-19 22:13:10 +0200

    labc_inference_output_Fwith4and5moree2_4vis

--- projects/datasets/robota.py
+++ projects/datasets/robota.py
@@ -17,3 +17,21 @@ class RobotaDataset(CocoDataset):
                'AR09 Lahey Traction Forceps 15.8',
                'AR14 Septum Forceps Hartman 20.0')
 
+    PALETTE = [    (211, 47, 47),    # Softer red
+    (56, 142, 60),    # Softer green
+    (25, 118, 210),   # Softer blue
+    (251, 192, 45),   # Softer yellow
+    (245, 124, 0),    # Softer orange
+    (123, 31, 162),   # Softer purple
+    (194, 24, 91),    # Softer pink
+    (0, 172, 193),    # Softer cyan
+    (216, 27, 96),    # Softer magenta
+    (175, 180, 43),   # Softer lime
+    (0, 121, 107),    # Softer teal
+    (255, 160, 0),    # Softer gold
+    (255, 112, 67),   # Softer coral
+    (94, 53, 177),    # Softer violet
+    (198, 40, 40),    # Softer crimson
+    (67, 160, 71),    # Softer spring green
+    ]
+
