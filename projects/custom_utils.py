4b6be4dd0d8683064b3be94f9c4807395adf1c3b
aelron <asaf.elron@gmail.com>
2024-12-01 13:56:38 +0200

    towards fetch_clearml_datasets [never tested!]

--- projects/custom_utils.py
+++ projects/custom_utils.py
@@ -6,7 +6,9 @@ from clearml import Dataset
 
 
 def fetch_clearml_datasets(cfg, logger=None):
-    """Fetch datasets from ClearML and update the paths"""
+    """Fetch datasets from ClearML and update the paths
+    Format of mock ClearML URI: clearml://<project>/<dataset_name>:<dataset_version>/<internal_path>
+    """
 
     assert isinstance(cfg, mmcv.Config), \
         f'cfg got wrong type: {type(cfg)}, expected mmcv.Config'
@@ -16,16 +18,16 @@ def fetch_clearml_datasets(cfg, logger=None):
             return mock_clearml_uri
 
         try:
-            dataset_name_and_version, internal_path = mock_clearml_uri.lstrip('clearml://').split('/', 2)
+            project_name, dataset_name_and_version, internal_path = mock_clearml_uri.lstrip('clearml://').split('/', 3)
             dataset_name, dataset_version = dataset_name_and_version.split(':')
         except Exception as e:
             raise ValueError(f'Invalid mock ClearML URI: {mock_clearml_uri}')
 
-        # TODO: also get dataset_project from the mock URI!
-        dataset = Dataset.get(dataset_project='SurgicalTools', dataset_name=dataset_name, dataset_version=dataset_version)
+        # TODO: cache the call to Dataset.get
+        dataset = Dataset.get(dataset_project=project_name, dataset_name=dataset_name, dataset_version=dataset_version)
         dataset_path = dataset.get_local_copy()
-        if isinstance(dataset_path, str) and dataset_path:  # Ensure it's a valid non-empty string
-            return dataset_path
+        if not isinstance(dataset_path, str):
+            raise ValueError(f'Failed to fetch dataset: {dataset_name} version: {dataset_version} from project: {project_name}')
 
         local_uri = os.path.join(dataset_path, internal_path)
 
