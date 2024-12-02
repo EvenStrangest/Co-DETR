513579226b8b1083ddfc41c4b389387479ad8883
aelron <asaf.elron@gmail.com>
2024-12-02 06:20:25 +0200

    cosmetics

--- tools/download_dataset_from_clearml.py
+++ tools/download_dataset_from_clearml.py
@@ -1,20 +1,21 @@
 from clearml import Dataset
 
 
-def get_valid_dataset_path():
-    dataset_name, dataset_version = 'RobotA_with_Photomontage_Round5', '1.0.0'
+def get_valid_dataset_path(project: str, dataset_name: str, dataset_version: str):
 
+    tries_so_far = 0
     while True:
         try:
-            robota = Dataset.get(dataset_project='SurgicalTools', dataset_name=dataset_name,
-                                 dataset_version=dataset_version)
+            tries_so_far += 1
+            print(f"Attempt #{tries_so_far} to get the dataset path ...")
+            robota = Dataset.get(dataset_project=project, dataset_name=dataset_name, dataset_version=dataset_version)
             dataset_path = robota.get_local_copy()
-            if isinstance(dataset_path, str) and dataset_path:  # Ensure it's a valid non-empty string
+            if isinstance(dataset_path, str) and dataset_path:
+                print(f"Dataset path: {dataset_path}")
                 return dataset_path
         except Exception as e:
             print(f"Error occurred: {e}. Retrying...")
 
 
 # Usage
-dataset_path = get_valid_dataset_path()
-print(f"Valid dataset path: {dataset_path}")
+dataset_path = get_valid_dataset_path('SurgicalTools', 'RobotA_with_Photomontage_Round5', '1.0.0')
